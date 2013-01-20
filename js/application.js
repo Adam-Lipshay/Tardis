53d0d513b621048c19b9a1819335df331b392d6f
Philip Dodds <philip.dodds@me.com>
2013-01-20 11:20:53 -0500

    Got the rotation in

--- js/application.js
+++ js/application.js
@@ -1,71 +1,138 @@
-var camera, scene, renderer, cubeMesh, cylinderMesh, material, mesh, controls;
+//----Variables----//
+//DOM element to attach the renderer to
+var viewport;
 
+//built-in three.js controls will be attached to this
+var controls;
 
+//viewport size
+var viewportWidth = 800;
+var viewportHeight = 600;
 
-init();
-animate();
+//camera attributes
+var view_angle = 45,
+    aspect = viewportWidth / viewportHeight,
+    near = 0.1,
+    //near clip-plane
+    far = 10000; //far clip-plane
+//sphere specifications
+var radius = 50,
+    segments = 32,
+    rings = 32;
 
-function init() {
+var tardis;
+
+var rotation = 0;
+
+var rotationSpeed = 0.01;
+
+//----Constructors----//
+var renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(
+view_angle, aspect, near, far);
+
+//constructs an instance of a white light
+var pointLight = new THREE.HemisphereLight();
+
+//a cross-browser method for efficient animation, more info at:
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+window.requestAnimFrame = (function() {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+    function(callback) {
+        window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+//----Initialization----//
+
+function initialize() {
+    //Sets up the renderer to the same size as a DOM element
+    //and attaches it to that element
+    renderer.setSize(viewportWidth, viewportHeight);
+    viewport = document.getElementById('viewport');
+    viewport.appendChild(renderer.domElement);
 
     
+    
 
-    scene = new THREE.Scene();
+    //attaches fly controls to the camera
+    controls = new THREE.FlyControls(camera);
+    //camera control properties
+    controls.movementSpeed = 1;
+    controls.domElement = viewport;
+    controls.rollSpeed = 0.01;
+    controls.autoForward = false;
+    controls.dragToLook = true;
 
-    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 200;
-    camera.position.x = 110;
-    camera.position.y = 0;
+
+    //pointLight.position.set(10, 50, 150);
+
+    //add the objects to the scene
     scene.add(camera);
+    scene.add(pointLight);
 
-    // cubeGeometry = new THREE.CubeGeometry(200, 200, 200);
-    // cylinderGeometry = new THREE.CylinderGeometry(50,50,50)
+    camera.position.set(428,428,428);
 
-    // material = new THREE.MeshLambertMaterial(
-    // {
-    //   color: 0x102372
-    // });
-
-    // cubeMesh = new THREE.Mesh(cubeGeometry, material);
-    // cylinderMesh = new THREE.Mesh(cylinderGeometry,material)
-    // cylinderMesh.position.set( 0, 100,0 );
-    // scene.add(cubeMesh);
-    // scene.add(cylinderMesh);
-
-    // 
-    // Adam - once you have a web server running you can use this 
-    //
     var loader = new THREE.ColladaLoader();
-    loader.load('tardis.dae', function (result) {
-      scene.add(result.scene);
+    loader.load('tardis.dae', function(result) {
+        tardis = result.scene;
+        scene.add(tardis);
+        update();
+
+        window.setTimeout(firstChange,5000);
+        window.setTimeout(secondChange,10000);
     });
 
+    
+    
+}
 
-    // Adam you need to fix this so you can fly around the tardis 
-    //
-    // controls = new THREE.FlyControls(new THREE.Object3D());
+function firstChange() {
 
-    renderer = new THREE.CanvasRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    document.body.appendChild(renderer.domElement);
+    rotationSpeed = -0.05;
 
 }
 
-function animate() {
+function secondChange() {
 
-    requestAnimationFrame(animate);
-    render();
+    rotationSpeed = 0.07;
 
 }
 
-function render() {
 
-    // cubeMesh.rotation.x += 0.00;
-    // cubeMesh.rotation.y += 0.01;
 
-    // cylinderMesh.rotation.x += 0.00;
-    // cylinderMesh.rotation.y += 0.01;
+//----Update----//
 
+function update() {
+    //requests the browser to call update at it's own pace
+    requestAnimFrame(update);
+
+    //update controls
+    controls.update(1);
+
+    // Lets try moving the camera
+    tardis.rotation.x = rotation;
+    tardis.rotation.y = rotation;
+    tardis.rotation.z = rotation;
+
+    rotation = rotation + rotationSpeed;
+
+    console.log(camera.position);
+
+    camera.lookAt(scene.position);
+
+    //call draw
+    draw();
+}
+
+
+
+//----Draw----//
+
+function draw() {
     renderer.render(scene, camera);
-
 }
