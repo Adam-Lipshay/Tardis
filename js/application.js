5383a938c2fb03e495c573df31b617cf8df8ef4d
OpenShift guest <adamlipshay@hotmail.com>
2013-02-17 11:36:03 -0500

    background editing

--- js/application.js
+++ js/application.js
@@ -30,10 +30,11 @@ var rotationSpeed = 0.01;
 var renderer = new THREE.WebGLRenderer({
     antialias: true
 });
+renderer.setClearColorHex( 0x000000, 1 );
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(
-view_angle, aspect, near, far);
+40, window.innerWidth / window.innerHeight, 0.1, 10000 );
 
 //constructs an instance of a white light
 var pointLight = new THREE.HemisphereLight();
@@ -52,21 +53,9 @@ window.requestAnimFrame = (function() {
 function initialize() {
     //Sets up the renderer to the same size as a DOM element
     //and attaches it to that element
-    renderer.setSize(viewportWidth, viewportHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
     viewport = document.getElementById('viewport');
     viewport.appendChild(renderer.domElement);
-
-    
-    
-
-    //attaches fly controls to the camera
-    controls = new THREE.FlyControls(camera);
-    //camera control properties
-    controls.movementSpeed = 1;
-    controls.domElement = viewport;
-    controls.rollSpeed = 0.01;
-    controls.autoForward = false;
-    controls.dragToLook = true;
 
 
     //pointLight.position.set(10, 50, 150);
@@ -75,8 +64,37 @@ function initialize() {
     scene.add(camera);
     scene.add(pointLight);
 
-    camera.position.set(428,428,428);
+    camera.position.set(0, 0, 700);
 
+	var light	= new THREE.DirectionalLight( 0xff8000, 1.5 );
+	light.position.set( 1, 1, 200 ).normalize();
+	scene.add( light );
+	
+	var light	= new THREE.DirectionalLight( 0xff8000, 1.5 );
+	light.position.set( -1, 1, 200 ).normalize();
+	scene.add( light );
+	
+	var light	= new THREE.PointLight( 0x44FFAA, 15, 25 );
+	light.position.set( 0, -3, 200 );
+	scene.add( light );
+	
+	var light	= new THREE.PointLight( 0xff4400, 20, 30 );
+	light.position.set( 3, 3, 200 );
+	scene.add( light );
+	
+	scene.fog	= new THREE.FogExp2( 0x000000, 0.001 );
+
+	// here you add your objects
+	// - you will most likely replace this part by your own
+	var geometry	= new THREE.CylinderGeometry( 100, 100, 3000, 3200, 100, true );
+	texture		= THREE.ImageUtils.loadTexture( "images/water.jpg" );
+	texture.wrapT	= THREE.RepeatWrapping;
+
+	var material	= new THREE.MeshLambertMaterial({color : 0xFFFFFF, map : texture});
+	var mesh	= new THREE.Mesh( geometry, material );
+	mesh.rotation.x	= Math.PI/2;
+	scene.add( mesh );
+    mesh.flipSided    = true;
     var loader = new THREE.ColladaLoader();
     loader.load('tardis.dae', function(result) {
         tardis = result.scene;
@@ -87,8 +105,6 @@ function initialize() {
         window.setTimeout(secondChange,10100);
     });
 
-    
-    
 }
 
 function firstChange() {
@@ -112,20 +128,32 @@ function update() {
     //requests the browser to call update at it's own pace
     requestAnimFrame(update);
 
-    //update controls
-    controls.update(1);
 
     // Lets try moving the camera
     tardis.rotation.x = rotation;
-//    tardis.rotation.y = rotation;
-//    tardis.rotation.z = rotation;
+    tardis.rotation.y = rotation;
+    tardis.rotation.z = rotation;
 
     rotation = rotation + rotationSpeed;
 
     console.log(camera.position);
 
     camera.lookAt(scene.position);
+//background
 
+	// move the texture to give the illusion of moving thru the tunnel
+	texture.offset.y	+= 0.008;
+	texture.offset.y	%= 1;
+	texture.needsUpdate	= true;
+
+	// move the camera back and forth
+	var seconds		= Date.now() / 1000;
+	var radius		= 0.70;
+	var angle		= Math.sin(0.75 * seconds * Math.PI) / 4;
+	//angle	= (seconds*Math.PI)/4;
+	camera.position.x	= Math.cos(angle - Math.PI/2) * radius;
+	camera.position.y	= Math.sin(angle - Math.PI/2) * radius;
+	camera.rotation.z	= angle;
     //call draw
     draw();
 }
